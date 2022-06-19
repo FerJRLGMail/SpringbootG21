bea139ab999d5b14b8842c02a641f2743559fd91
ceciliazurla <100606122+ceciliazurla@users.noreply.github.com>
2022-06-19 22:00:16 +0200

    Add files via upload

---
+++ src/main/java/es/taw/tawebayspringbootgrupo21/service/ProductoService.java
@@ -0,0 +1,24 @@
+package es.taw.tawebayspringbootgrupo21.service;
+
+import es.taw.tawebayspringbootgrupo21.dao.ProductoRepository;
+import es.taw.tawebayspringbootgrupo21.entity.Producto;
+import es.taw.tawebayspringbootgrupo21.entity.Puja;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class ProductoService {
+    @Autowired
+    private ProductoRepository pr;
+
+    public void modificaProducto(Producto producto, List<Puja> pujalist, BigDecimal precio)
+    {
+        producto.setPujasByProductId(pujalist);
+        producto.setPrecio(precio);
+
+        this.pr.save(producto);
+    }
+}
